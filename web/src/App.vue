80e31933a97e79e5e886b96b082d4670d3cf7dea
JeremyHungDev <rain850218@gmail.com>
2026-05-29 23:04:37 +0800

    feat(web): add Solarized Naive UI themeOverrides

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,16 +9,29 @@ import {
   darkTheme,
   zhTW,
   dateZhTW,
+  type GlobalThemeOverrides,
 } from 'naive-ui'
 import { useThemeStore } from '@/stores/theme'
 import AppShell from '@/components/AppShell.vue'
 
 const themeStore = useThemeStore()
 const theme = computed(() => (themeStore.dark ? darkTheme : null))
+
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor:        '#268bd2',
+    primaryColorHover:   '#2aa198',
+    primaryColorPressed: '#268bd2',
+    successColor:        '#859900',
+    warningColor:        '#b58900',
+    errorColor:          '#dc322f',
+    infoColor:           '#2aa198',
+  },
+}
 </script>
 
 <template>
-  <n-config-provider :theme="theme" :locale="zhTW" :date-locale="dateZhTW">
+  <n-config-provider :theme="theme" :theme-overrides="themeOverrides" :locale="zhTW" :date-locale="dateZhTW">
     <n-loading-bar-provider>
       <n-message-provider>
         <n-dialog-provider>
